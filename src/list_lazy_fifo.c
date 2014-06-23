6fc937af2a4e1d33fb22705838eec44b3928823f
Daniel Fiser <danfis@danfis.cz>
2014-06-23 21:32:44 +0200

    Lazy FIFO list is based on boruvka FIFO structure.

--- src/list_lazy_fifo.c
+++ src/list_lazy_fifo.c
@@ -1,17 +1,17 @@
+#include <unistd.h>
 #include <boruvka/alloc.h>
-#include <boruvka/list.h>
+#include <boruvka/fifo.h>
 #include "plan/list_lazy.h"
 
 struct _plan_list_lazy_fifo_t {
     plan_list_lazy_t list;
-    bor_list_t fifo;
+    bor_fifo_t *fifo;
 };
 typedef struct _plan_list_lazy_fifo_t plan_list_lazy_fifo_t;
 
 struct _plan_list_lazy_fifo_el_t {
     plan_state_id_t parent_state_id;
     plan_operator_t *op;
-    bor_list_t fifo;
 };
 typedef struct _plan_list_lazy_fifo_el_t plan_list_lazy_fifo_el_t;
 
@@ -29,14 +29,19 @@ static void planListLazyFifoClear(void *l);
 plan_list_lazy_t *planListLazyFifoNew(void)
 {
     plan_list_lazy_fifo_t *l;
+    size_t segment_size;
 
     l = BOR_ALLOC(plan_list_lazy_fifo_t);
-    borListInit(&l->fifo);
     planListLazyInit(&l->list,
                      planListLazyFifoDel,
                      planListLazyFifoPush,
                      planListLazyFifoPop,
                      planListLazyFifoClear);
+
+    segment_size = sysconf(_SC_PAGESIZE);
+    segment_size *= 4;
+    l->fifo = borFifoNewSize(sizeof(plan_list_lazy_fifo_el_t), segment_size);
+
 
     return &l->list;
 }
@@ -45,7 +50,7 @@ void planListLazyFifoDel(void *_l)
 {
     plan_list_lazy_fifo_t *l = _l;
     planListLazyFree(&l->list);
-    planListLazyFifoClear(l);
+    borFifoDel(l->fifo);
     BOR_FREE(l);
 }
 
@@ -55,13 +60,12 @@ static void planListLazyFifoPush(void *_l,
                                  plan_operator_t *op)
 {
     plan_list_lazy_fifo_t *l = _l;
-    plan_list_lazy_fifo_el_t *el;
+    plan_list_lazy_fifo_el_t el;
 
-    el = BOR_ALLOC(plan_list_lazy_fifo_el_t);
-    el->parent_state_id = parent_state_id;
-    el->op = op;
-    borListInit(&el->fifo);
-    borListAppend(&l->fifo, &el->fifo);
+    el.parent_state_id = parent_state_id;
+    el.op = op;
+
+    borFifoPush(l->fifo, &el);
 }
 
 static int planListLazyFifoPop(void *_l,
@@ -69,23 +73,18 @@ static int planListLazyFifoPop(void *_l,
                                plan_operator_t **op)
 {
     plan_list_lazy_fifo_t *l = _l;
-    bor_list_t *item;
     plan_list_lazy_fifo_el_t *el;
 
-    if (borListEmpty(&l->fifo))
+    if (borFifoEmpty(l->fifo))
         return -1;
 
-    // get next element from front of the list
-    item = borListNext(&l->fifo);
-    borListDel(item);
-    el = BOR_LIST_ENTRY(item, plan_list_lazy_fifo_el_t, fifo);
+    el = borFifoFront(l->fifo);
 
     // copy values to the output args
     *parent_state_id = el->parent_state_id;
     *op = el->op;
 
-    // free allocated memory
-    BOR_FREE(el);
+    borFifoPop(l->fifo);
 
     return 0;
 }
@@ -93,13 +92,5 @@ static int planListLazyFifoPop(void *_l,
 static void planListLazyFifoClear(void *_l)
 {
     plan_list_lazy_fifo_t *l = _l;
-    bor_list_t *item;
-    plan_list_lazy_fifo_el_t *el;
-
-    while (!borListEmpty(&l->fifo)){
-        item = borListNext(&l->fifo);
-        borListDel(item);
-        el = BOR_LIST_ENTRY(item, plan_list_lazy_fifo_el_t, fifo);
-        BOR_FREE(el);
-    }
+    borFifoClear(l->fifo);
 }

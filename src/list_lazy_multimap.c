bc56aed256c034e71536f74785e7731b0c64cf7b
Daniel Fiser <danfis@danfis.cz>
2014-06-26 17:13:29 +0200

    list-lazy-multimap: Reimplemented with fifo as underlying structure.
    This seems to be a bit slower but consumest much less memory.

--- src/list_lazy_multimap.c
+++ src/list_lazy_multimap.c
@@ -1,19 +1,25 @@
 #include <boruvka/alloc.h>
-#include <boruvka/multimap_int.h>
+#include <boruvka/rbtree_int.h>
+#include <boruvka/fifo.h>
 #include "plan/list_lazy.h"
 
 struct _plan_list_lazy_multimap_t {
     plan_list_lazy_t list;
-    bor_multimap_int_t *map;
+    bor_rbtree_int_t tree;
 };
 typedef struct _plan_list_lazy_multimap_t plan_list_lazy_multimap_t;
 
 struct _node_t {
     plan_state_id_t parent_state_id;
     plan_operator_t *op;
-    bor_multimap_int_node_t node;
 };
 typedef struct _node_t node_t;
+
+struct _keynode_t {
+    bor_fifo_t fifo;
+    bor_rbtree_int_node_t tree;
+};
+typedef struct _keynode_t keynode_t;
 
 static void planListLazyMultiMapDel(void *);
 static void planListLazyMultiMapPush(void *,
@@ -31,7 +37,8 @@ plan_list_lazy_t *planListLazyMultiMapNew(void)
     plan_list_lazy_multimap_t *l;
 
     l = BOR_ALLOC(plan_list_lazy_multimap_t);
-    l->map = borMultiMapIntNew();
+    borRBTreeIntInit(&l->tree);
+
     planListLazyInit(&l->list,
                      planListLazyMultiMapDel,
                      planListLazyMultiMapPush,
@@ -45,7 +52,7 @@ static void planListLazyMultiMapDel(void *_l)
 {
     plan_list_lazy_multimap_t *l = _l;
     planListLazyMultiMapClear(l);
-    borMultiMapIntDel(l->map);
+    borRBTreeIntFree(&l->tree);
     BOR_FREE(l);
 }
 
@@ -55,12 +62,22 @@ static void planListLazyMultiMapPush(void *_l,
                                      plan_operator_t *op)
 {
     plan_list_lazy_multimap_t *l = _l;
-    node_t *n;
+    node_t n;
+    keynode_t *keynode;
+    bor_rbtree_int_node_t *kn;
 
-    n = BOR_ALLOC(node_t);
-    n->parent_state_id = parent_state_id;
-    n->op              = op;
-    borMultiMapIntInsert(l->map, cost, &n->node);
+    keynode = BOR_ALLOC(keynode_t);
+    borFifoInit(&keynode->fifo, sizeof(node_t));
+    kn = borRBTreeIntInsert(&l->tree, cost, &keynode->tree);
+    if (kn != NULL){
+        borFifoFree(&keynode->fifo);
+        BOR_FREE(keynode);
+        keynode = bor_container_of(kn, keynode_t, tree);
+    }
+
+    n.parent_state_id = parent_state_id;
+    n.op = op;
+    borFifoPush(&keynode->fifo, &n);
 }
 
 static int planListLazyMultiMapPop(void *_l,
@@ -68,18 +85,26 @@ static int planListLazyMultiMapPop(void *_l,
                                    plan_operator_t **op)
 {
     plan_list_lazy_multimap_t *l = _l;
-    bor_multimap_int_node_t *map_node;
     node_t *n;
+    keynode_t *keynode;
+    bor_rbtree_int_node_t *kn;
 
-    if (borMultiMapIntEmpty(l->map))
+    if (borRBTreeIntEmpty(&l->tree))
         return -1;
 
-    map_node = borMultiMapIntExtractMinNodeFifo(l->map, NULL);
-    n = bor_container_of(map_node, node_t, node);
+    kn = borRBTreeIntMin(&l->tree);
+    keynode = bor_container_of(kn, keynode_t, tree);
 
+    n = borFifoFront(&keynode->fifo);
     *parent_state_id = n->parent_state_id;
     *op              = n->op;
-    BOR_FREE(n);
+    borFifoPop(&keynode->fifo);
+
+    if (borFifoEmpty(&keynode->fifo)){
+        borRBTreeIntRemove(&l->tree, &keynode->tree);
+        borFifoFree(&keynode->fifo);
+        BOR_FREE(keynode);
+    }
 
     return 0;
 }
@@ -87,9 +112,13 @@ static int planListLazyMultiMapPop(void *_l,
 static void planListLazyMultiMapClear(void *_l)
 {
     plan_list_lazy_multimap_t *l = _l;
-    plan_state_id_t sid;
-    plan_operator_t *op;
+    bor_rbtree_int_node_t *kn, *tmp;
+    keynode_t *keynode;
 
-    while (planListLazyMultiMapPop(l, &sid, &op) != -1);
+    BOR_RBTREE_INT_FOR_EACH_SAFE(&l->tree, kn, tmp){
+        borRBTreeIntRemove(&l->tree, kn);
+        keynode = bor_container_of(kn, keynode_t, tree);
+        borFifoFree(&keynode->fifo);
+        BOR_FREE(keynode);
+    }
 }
-

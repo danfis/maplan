67d2033bd48c8a96e55b57a5b7adbd2ce1ed17c5
Daniel Fiser <danfis@danfis.cz>
2014-06-26 18:11:28 +0200

    list-lazy-multimap: Added comments.

--- src/list_lazy_multimap.c
+++ src/list_lazy_multimap.c
@@ -3,21 +3,24 @@
 #include <boruvka/fifo.h>
 #include "plan/list_lazy.h"
 
+/** A structure containing a stored value */
 struct _node_t {
     plan_state_id_t parent_state_id;
     plan_operator_t *op;
 };
 typedef struct _node_t node_t;
 
+/** A node holding a key and all the values. */
 struct _keynode_t {
-    bor_fifo_t fifo;
-    bor_splaytree_int_node_t tree;
+    bor_fifo_t fifo;               /*!< Structure containing all values */
+    bor_splaytree_int_node_t tree; /*!< Connector to the tree */
 };
 typedef struct _keynode_t keynode_t;
 
+/** A main structure */
 struct _plan_list_lazy_multimap_t {
-    plan_list_lazy_t list;
-    bor_splaytree_int_t tree;
+    plan_list_lazy_t list;    /*!< Parent class */
+    bor_splaytree_int_t tree; /*!< Instance of a tree */
     keynode_t *pre_keynode;   /*!< Preinitialized key-node */
 };
 typedef struct _plan_list_lazy_multimap_t plan_list_lazy_multimap_t;
@@ -74,15 +77,22 @@ static void planListLazyMultiMapPush(void *_l,
     keynode_t *keynode;
     bor_splaytree_int_node_t *kn;
 
+    // Try tu push the preinitialized keynode with the cost as a key
     kn = borSplayTreeIntInsert(&l->tree, cost, &l->pre_keynode->tree);
+
     if (kn == NULL){
+        // The insertion was successful, so remember the inserted key-node
+        // and preinitialize a next one.
         keynode = l->pre_keynode;
         l->pre_keynode = BOR_ALLOC(keynode_t);
         borFifoInit(&l->pre_keynode->fifo, sizeof(node_t));
+
     }else if (kn != NULL){
+        // Key already in tree
         keynode = bor_container_of(kn, keynode_t, tree);
     }
 
+    // Set up the actual values and insert it into key-node.
     n.parent_state_id = parent_state_id;
     n.op = op;
     borFifoPush(&keynode->fifo, &n);
@@ -100,14 +110,19 @@ static int planListLazyMultiMapPop(void *_l,
     if (borSplayTreeIntEmpty(&l->tree))
         return -1;
 
+    // Get the minimal key-node
     kn = borSplayTreeIntMin(&l->tree);
     keynode = bor_container_of(kn, keynode_t, tree);
 
+    // We know for sure that this key-node must contain some values because
+    // an empty key-nodes are removed.
+    // Pop the values from the key-node.
     n = borFifoFront(&keynode->fifo);
     *parent_state_id = n->parent_state_id;
     *op              = n->op;
     borFifoPop(&keynode->fifo);
 
+    // If the key-node is empty, remove it from the tree
     if (borFifoEmpty(&keynode->fifo)){
         borSplayTreeIntRemove(&l->tree, &keynode->tree);
         borFifoFree(&keynode->fifo);

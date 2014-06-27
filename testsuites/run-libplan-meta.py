d96b6ad30167ff4a43a2aa83ec6691960e246433
Daniel Fiser <danfis@danfis.cz>
2014-06-27 12:21:59 +0200

    Added scripts for benchmarking on metacentrum.

--- testsuites/run-libplan-meta.py
+++ testsuites/run-libplan-meta.py
@@ -4,33 +4,38 @@ import sys
 import os
 
 REPEATS = 5
-FD_SEARCH = (
-    'ehc(goalcount())',
-    'ehc(add())',
-    'ehc(hmax())',
-    'ehc(ff())',
+SEARCH = (
+    ['-s', 'ehc', '-H', 'goalcount'],
+    ['-s', 'ehc', '-H', 'add'],
+    ['-s', 'ehc', '-H', 'max'],
+    ['-s', 'ehc', '-H', 'ff'],
 
-    'lazy(single(goalcount()))',
-    'lazy(single(add()))',
-    'lazy(single(hmax()))',
-    'lazy(single(ff()))',
+    ['-s', 'lazy', '-l', 'heap', '-H', 'goalcount'],
+    ['-s', 'lazy', '-l', 'heap', '-H', 'add'],
+    ['-s', 'lazy', '-l', 'heap', '-H', 'max'],
+    ['-s', 'lazy', '-l', 'heap', '-H', 'ff'],
 
-    'lazy(single_buckets(goalcount()))',
-    'lazy(single_buckets(add()))',
-    'lazy(single_buckets(hmax()))',
-    'lazy(single_buckets(ff()))',
+    ['-s', 'lazy', '-l', 'bucket', '-H', 'goalcount'],
+    ['-s', 'lazy', '-l', 'bucket', '-H', 'add'],
+    ['-s', 'lazy', '-l', 'bucket', '-H', 'max'],
+    ['-s', 'lazy', '-l', 'bucket', '-H', 'ff'],
+    ['-s', 'lazy', '-l', 'map', '-H', 'goalcount'],
+
+    ['-s', 'lazy', '-l', 'map', '-H', 'add'],
+    ['-s', 'lazy', '-l', 'map', '-H', 'max'],
+    ['-s', 'lazy', '-l', 'map', '-H', 'ff'],
 )
 
-META_DIR = '/storage/praha1/home/danfis/fd-output/'
+META_DIR = '/storage/praha1/home/danfis/libplan-output/'
 
 META_RUN = '''#!/bin/bash
 #PBS -l nodes=1:ppn=1:xeon:debian7:nfs4:cl_luna:nodecpus16
-#PBS -l walltime=2h
+#PBS -l walltime=3h
 #PBS -l mem=8gb
 #PBS -l scratch=400mb:local
 
 PROBLEM_SAS="/storage/praha1/home/danfis/libplan/data/ma-benchmarks/{sas}"
-DOWNWARD="/storage/praha1/home/danfis/fast-downward2/src/search/downward-release"
+BIN="/storage/praha1/home/danfis/libplan/testsuites/search"
 LOGFILE="{workdir}/log"
 
 WORK_LOGFILE="$SCRATCHDIR/log"
@@ -40,15 +45,15 @@ trap "cp $WORK_LOGFILE $LOGFILE.0; rm -r $SCRATCHDIR" TERM EXIT
 cp $PROBLEM_SAS $SCRATCHDIR/sas
 
 echo '==RUN {search} ==' >$WORK_LOGFILE
-$DOWNWARD --search '{search}' <$SCRATCHDIR/sas >>$WORK_LOGFILE 2>&1
+$BIN {search} --max-mem 4000000 -p $SCRATCHDIR/sas >>$WORK_LOGFILE 2>&1
 echo '==RUN {search} ==' >>$WORK_LOGFILE
-$DOWNWARD --search '{search}' <$SCRATCHDIR/sas >>$WORK_LOGFILE 2>&1
+$BIN {search} --max-mem 4000000 -p $SCRATCHDIR/sas >>$WORK_LOGFILE 2>&1
 echo '==RUN {search} ==' >>$WORK_LOGFILE
-$DOWNWARD --search '{search}' <$SCRATCHDIR/sas >>$WORK_LOGFILE 2>&1
+$BIN {search} --max-mem 4000000 -p $SCRATCHDIR/sas >>$WORK_LOGFILE 2>&1
 echo '==RUN {search} ==' >>$WORK_LOGFILE
-$DOWNWARD --search '{search}' <$SCRATCHDIR/sas >>$WORK_LOGFILE 2>&1
+$BIN {search} --max-mem 4000000 -p $SCRATCHDIR/sas >>$WORK_LOGFILE 2>&1
 echo '==RUN {search} ==' >>$WORK_LOGFILE
-$DOWNWARD --search '{search}' <$SCRATCHDIR/sas >>$WORK_LOGFILE 2>&1
+$BIN {search} --max-mem 4000000 -p $SCRATCHDIR/sas >>$WORK_LOGFILE 2>&1
 
 cp $WORK_LOGFILE $LOGFILE
 '''
@@ -64,9 +69,9 @@ def loadSasDir(domain, dr):
     sas = []
 
     files = os.listdir(dr)
-    files = filter(lambda x: x[-4:] == '.sas', files)
+    files = filter(lambda x: x[-5:] == '.json', files)
     for f in files:
-        problem = f[:-4]
+        problem = f[:-5]
         sfile = domain + '/' + f
         sas += [Sas(domain, problem, sfile)]
 
@@ -83,14 +88,14 @@ def loadSas(topdir):
     return sas
 
 def run(sas):
-    for i in range(len(FD_SEARCH)):
+    for i in range(len(SEARCH)):
         workdir = '{0}/{1}/{2}/search-{3}'
         workdir = workdir.format(META_DIR, sas.domain, sas.problem, i)
         cmd = 'ssh meta-brno \'mkdir -p {0}\''.format(workdir)
         print('CMD:', cmd)
         os.system(cmd)
 
-        search = FD_SEARCH[i]
+        search = ' '.join(SEARCH[i])
         run_sh = META_RUN.format(search = search, workdir = workdir,
                                  sas = sas.sas)
         with open('/tmp/run.sh', 'w') as fout:

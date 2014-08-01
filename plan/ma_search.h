1a978d556561e8bd35ad9861537f8ab911a5d845
Daniel Fiser <danfis@danfis.cz>
2014-08-01 13:00:23 +0200

    ma-search: Parent class.

--- plan/ma_search.h
+++ plan/ma_search.h
@@ -12,7 +12,21 @@
 /** Forward declaration */
 typedef struct _plan_ma_search_t plan_ma_search_t;
 
+typedef int (*plan_ma_search_init_step_fn)(plan_ma_agent_t *agent,
+                                           plan_ma_search_t *search);
+
+typedef int (*plan_ma_search_step_fn)(plan_ma_agent_t *agent,
+                                      plan_ma_search_t *search);
+
+typedef int (*plan_ma_search_update_fn)(plan_ma_agent_t *agent,
+                                        plan_ma_search_t *search,
+                                        const plan_ma_msg_t *msg);
+
 struct _plan_ma_search_t {
+    plan_ma_search_init_step_fn init_step_fn;
+    plan_ma_search_step_fn step_fn;
+    plan_ma_search_update_fn update_fn;
+
     plan_state_pool_t *state_pool;
     plan_state_space_t *state_space;
 };
@@ -23,5 +37,12 @@ int planMASearchInitStep(plan_ma_agent_t *agent, plan_ma_search_t *search);
 int planMASearchStep(plan_ma_agent_t *agent, plan_ma_search_t *search);
 int planMASearchUpdate(plan_ma_agent_t *agent, plan_ma_search_t *search,
                        const plan_ma_msg_t *msg);
+
+
+
+void _planMASearchInit(plan_ma_search_t *search,
+                       plan_ma_search_init_step_fn init_step_fn,
+                       plan_ma_search_step_fn step_fn,
+                       plan_ma_search_update_fn update_fn);
 
 #endif /* __PLAN_MA_SEARCH_H__ */
--- src/ma_search.c
+++ src/ma_search.c
@@ -1,22 +1,28 @@
 #include "plan/ma_search.h"
 
-plan_ma_search_t *planMASearchEHCNew(void)
-{
-    return NULL;
-}
-
 int planMASearchInitStep(plan_ma_agent_t *agent, plan_ma_search_t *search)
 {
-    return 0;
+    return search->init_step_fn(agent, search);
 }
 
 int planMASearchStep(plan_ma_agent_t *agent, plan_ma_search_t *search)
 {
-    return 0;
+    return search->step_fn(agent, search);
 }
 
 int planMASearchUpdate(plan_ma_agent_t *agent, plan_ma_search_t *search,
                        const plan_ma_msg_t *msg)
 {
-    return 0;
+    return search->update_fn(agent, search, msg);
+}
+
+void _planMASearchInit(plan_ma_search_t *search,
+                       plan_ma_search_init_step_fn init_step_fn,
+                       plan_ma_search_step_fn step_fn,
+                       plan_ma_search_update_fn update_fn)
+{
+    bzero(search, sizeof(*search));
+    search->init_step_fn = init_step_fn;
+    search->step_fn = step_fn;
+    search->update_fn = update_fn;
 }

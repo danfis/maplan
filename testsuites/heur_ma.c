8e16a25845861b37ba8ca1b7d7283397efb47a4a
Daniel Fiser <danfis@danfis.cz>
2014-12-16 14:15:12 +0100

    Testsuite for ma heuristics.

--- testsuites/heur_ma.c
+++ testsuites/heur_ma.c
@@ -4,6 +4,7 @@
 
 #include "state_pool.h"
 
+typedef plan_heur_t *(*heur_new_fn)(const plan_problem_t *prob);
 
 static plan_ma_msg_t *nextMsg(plan_ma_comm_t **comm,
                               int agent_size, int *agent_id)
@@ -23,23 +24,25 @@ static plan_ma_msg_t *nextMsg(plan_ma_comm_t **comm,
 
     return NULL;
 }
-static int testMAHeur(plan_heur_t **heur, plan_ma_comm_t **comm,
-                      int agent_size, int agent_id,
-                      const plan_state_t *state, plan_cost_t target)
+
+static void testMAHeur(plan_heur_t **heur, plan_ma_comm_t **comm,
+                       int agent_size, int agent_id,
+                       const plan_state_t *state)
 {
     plan_heur_res_t res;
     plan_ma_msg_t *msg;
     int msg_agent_id;
     int status;
+    int i;
 
     planHeurResInit(&res);
     status = planHeurMA(heur[agent_id], comm[agent_id], state, &res);
     while (status != 0){
         msg = nextMsg(comm, agent_size, &msg_agent_id);
-        if (planMAMsgIsHeurMaxRequest(msg)){
+        if (planMAMsgHeurType(msg) == PLAN_MA_MSG_HEUR_REQUEST){
             planHeurMARequest(heur[msg_agent_id], comm[msg_agent_id], msg);
 
-        }else if (planMAMsgIsHeurMaxResponse(msg)){
+        }else if (planMAMsgHeurType(msg) == PLAN_MA_MSG_HEUR_UPDATE){
             status = planHeurMAUpdate(heur[msg_agent_id], comm[msg_agent_id],
                                       msg, &res);
 
@@ -47,40 +50,36 @@ static int testMAHeur(plan_heur_t **heur, plan_ma_comm_t **comm,
             fprintf(stderr, "Error: Unexpected message: %d\n",
                     planMAMsgType(msg));
             assertTrue(0);
-            return -1;
+            return;
         }
 
         planMAMsgDel(msg);
     }
 
-    assertEquals(res.heur, target);
-    if (res.heur != target)
-        return res.heur;
-    return -1;
+    printf("%d", (int)res.heur);
+    for (i = 0; i < planStateSize(state); ++i){
+        printf(" %d", planStateGet(state, i));
+    }
+    printf("\n");
 }
 
-static void testHeurMAMaxProb(const plan_problem_agents_t *p,
-                              state_pool_t *state_pool)
+static void testHeurMAProb(const plan_problem_agents_t *p,
+                           state_pool_t *state_pool,
+                           heur_new_fn heur_new)
 {
     int agent_size = p->agent_size;
     plan_heur_t *ma_heur[agent_size];
-    plan_heur_t *seq_heur;
     plan_state_t *seq_state;
     plan_ma_comm_queue_pool_t *comm_pool;
     plan_ma_comm_t *comm[agent_size];
-    int i, line, r;
-    plan_heur_res_t res;
+    int i, line;
 
     comm_pool = planMACommQueuePoolNew(agent_size);
     for (i = 0; i < agent_size; ++i){
-        ma_heur[i] = planHeurMARelaxMaxNew(p->agent + i);
+        ma_heur[i] = heur_new(p->agent + i);
         comm[i] = planMACommQueue(comm_pool, i);
     }
 
-    seq_heur = planHeurRelaxMaxNew(p->glob.var, p->glob.var_size,
-                                   p->glob.goal,
-                                   p->glob.op, p->glob.op_size,
-                                   p->glob.succ_gen);
     seq_state = planStateNew(p->glob.state_pool);
 
     for (i = 0; i < agent_size; ++i){
@@ -88,19 +87,12 @@ static void testHeurMAMaxProb(const plan_problem_agents_t *p,
         line = 0;
         while (statePoolNext(state_pool, seq_state) == 0){
             ++line;
-            planHeurResInit(&res);
-            planHeur(seq_heur, seq_state, &res);
-
-            r = testMAHeur(ma_heur, comm, agent_size, i, seq_state, res.heur);
-            if (r >= 0){
-                fprintf(stderr, "Failed with agent %d on %d'th line"
-                                " (target: %d, got: %d)\n",
-                        i, line, res.heur, r);
-            }
+            testMAHeur(ma_heur, comm, agent_size, i, seq_state);
+            if (line > 1000)
+                break;
         }
     }
 
-    planHeurDel(seq_heur);
     planStateDel(seq_state);
     for (i = 0; i < agent_size; ++i){
         planHeurDel(ma_heur[i]);
@@ -108,10 +100,13 @@ static void testHeurMAMaxProb(const plan_problem_agents_t *p,
     planMACommQueuePoolDel(comm_pool);
 }
 
-static void runTestHeurMAMax(const char *proto, const char *states)
+static void runTestHeurMA(const char *name, const char *proto,
+                          const char *states, heur_new_fn heur_new)
 {
     plan_problem_agents_t *p;
     state_pool_t state_pool;
+
+    printf("---- %s: %s ----\n", name, proto);
 
     p = planProblemAgentsFromProto(proto, PLAN_PROBLEM_USE_CG);
     if (p == NULL)
@@ -119,11 +114,25 @@ static void runTestHeurMAMax(const char *proto, const char *states)
 
     statePoolInit(&state_pool, states);
 
-    testHeurMAMaxProb(p, &state_pool);
+    testHeurMAProb(p, &state_pool, heur_new);
 
     statePoolFree(&state_pool);
     planProblemAgentsDel(p);
+
+    printf("---- %s: %s END ----\n", name, proto);
 }
+
+TEST(testHeurMAFF)
+{
+    runTestHeurMA("ma-ff", "../data/simple/pfile.proto",
+                  "states/simple.txt", planHeurMARelaxFFNew);
+    runTestHeurMA("ma-ff", "../data/ma-benchmarks/depot/pfile1.proto",
+                  "states/depot-pfile1.txt", planHeurMARelaxFFNew);
+    runTestHeurMA("ma-ff", "../data/ma-benchmarks/depot/pfile5.proto",
+                  "states/depot-pfile5.txt", planHeurMARelaxFFNew);
+}
+
+/*
 
 TEST(testHeurMAMaxSimple)
 {
@@ -153,3 +162,4 @@ TEST(testHeurMAMaxRoversP15){
     runTestHeurMAMax("../data/ma-benchmarks/rovers/p15.proto",
                      "states/rovers-p15.txt");
 }
+*/
--- testsuites/heur_ma.h
+++ testsuites/heur_ma.h
@@ -1,19 +1,25 @@
 #ifndef TEST_HEUR_MA_H
 #define TEST_HEUR_MA_H
 
+TEST(testHeurMAFF);
+/*
 TEST(testHeurMAMaxSimple);
 TEST(testHeurMAMaxDepotPfile1);
 TEST(testHeurMAMaxDepotPfile2);
 TEST(testHeurMAMaxRoversP03);
 TEST(testHeurMAMaxRoversP15);
+*/
 TEST(protobufTearDown);
 
 TEST_SUITE(TSHeurMA) {
+    TEST_ADD(testHeurMAFF),
+    /*
     TEST_ADD(testHeurMAMaxSimple),
     TEST_ADD(testHeurMAMaxDepotPfile1),
     TEST_ADD(testHeurMAMaxDepotPfile2),
     TEST_ADD(testHeurMAMaxRoversP03),
     TEST_ADD(testHeurMAMaxRoversP15),
+    */
     TEST_ADD(protobufTearDown),
     TEST_SUITE_CLOSURE
 };

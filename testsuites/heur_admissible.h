859c6fc293b4d1601b9d17b87fdccaf52cd82298
Daniel Fiser <danfis@danfis.cz>
2015-01-23 13:57:53 +0100

    Extended test of admissibility of heuristics.

--- testsuites/heur_admissible.h
+++ testsuites/heur_admissible.h
@@ -2,10 +2,12 @@
 #define TEST_HEUR_ADMISSIBLE_H
 
 TEST(testHeurAdmissibleLMCut);
+TEST(testHeurAdmissibleMax);
 TEST(protobufTearDown);
 
 TEST_SUITE(TSHeurAdmissible){
     TEST_ADD(testHeurAdmissibleLMCut),
+    TEST_ADD(testHeurAdmissibleMax),
     TEST_ADD(protobufTearDown),
     TEST_SUITE_CLOSURE
 };

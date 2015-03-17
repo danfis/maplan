2b4ea38f5a8ba460f6d330eab55a50c1cd38588d
Daniel Fiser <danfis@danfis.cz>
2015-03-17 16:14:10 +0100

    translate: Added script for translation of factored problems.

---
+++ third-party/translate/translate-factored.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+PYTHON=python2
+TRANSLATE="${DIR}/translate.py"
+
+size=0
+sizes=""
+domains=()
+problems=()
+agent_names=()
+agent_urls=""
+for domain in $(ls domain-*.pddl); do
+    agent_name=${domain##domain-}
+    agent_name=${agent_name%%.pddl}
+    problem=
+    for p in $(ls *-${agent_name}.pddl); do
+        if [ "$p" != "$domain" ]; then
+            problem="$p"
+            break
+        fi
+    done
+    echo $agent_name: $domain, $problem
+    domains+=($domain)
+    problems+=($problem)
+    agent_names+=($agent_name)
+    agent_urls="$agent_urls --agent-url ipc://translate-agent-$size"
+    sizes="$sizes $size"
+    size=$(($size + 1))
+done
+
+pids=""
+for i in $sizes; do
+    "$PYTHON" "$TRANSLATE" "${domains[$i]}" "${problems[$i]}" $@ \
+                           $agent_urls --agent-id $i \
+                           --output ${agent_names[$i]}.proto \
+                           --proto >${i}.log 2>&1 &
+    pids="$pids $!"
+done
+
+for pid in $pids; do
+    wait $pid
+done
+echo $size
+echo ${#domain[*]}

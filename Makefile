6c5af06bc23a46f99ff801c6f8a6453ac33623ee
Daniel Fiser <danfis@danfis.cz>
2015-04-17 21:05:38 +0200

    Added bsd-license note.

--- Makefile
+++ Makefile
@@ -1,18 +1,3 @@
-###
-# libplan
-# --------
-# Copyright (c)2014 Daniel Fiser <danfis@danfis.cz>
-#
-#  This file is part of boruvka.
-#
-#  Distributed under the OSI-approved BSD License (the "License");
-#  see accompanying file BDS-LICENSE for details or see
-#  <http://www.opensource.org/licenses/bsd-license.php>.
-#
-#  This software is distributed WITHOUT ANY WARRANTY; without even the
-#  implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-#  See the License for more information.
-##
 -include Makefile.local
 -include Makefile.include
 
--- plan/ma_comm.h
+++ plan/ma_comm.h
@@ -1,3 +1,22 @@
+/***
+ * maplan
+ * -------
+ * Copyright (c)2015 Daniel Fiser <danfis@danfis.cz>,
+ * Agent Technology Center, Department of Computer Science,
+ * Faculty of Electrical Engineering, Czech Technical University in Prague.
+ * All rights reserved.
+ *
+ * This file is part of maplan.
+ *
+ * Distributed under the OSI-approved BSD License (the "License");
+ * see accompanying file BDS-LICENSE for details or see
+ * <http://www.opensource.org/licenses/bsd-license.php>.
+ *
+ * This software is distributed WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the License for more information.
+ */
+
 #ifndef __PLAN_MA_COMM_H__
 #define __PLAN_MA_COMM_H__
 

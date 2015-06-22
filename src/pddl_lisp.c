cc6dd5ca123534476dc838e471fafe8e200a73b0
Daniel Fiser <danfis@danfis.cz>
2015-06-22 11:10:21 +0200

    pddl-lisp: Added keyword parsing.

--- src/pddl_lisp.c
+++ src/pddl_lisp.c
@@ -12,33 +12,114 @@
 #define IS_WS(c) ((c) == ' ' || (c) == '\n' || (c) == '\r' || (c) == '\t')
 #define IS_ALPHA(c) (!IS_WS(c) && (c) != ')' && (c) != '(' && (c) != ';')
 
-static plan_lisp_node_t *lispNodeNew(void)
-{
-    plan_lisp_node_t *n;
+struct _kw_t {
+    const char *text;
+    int kw;
+};
+typedef struct _kw_t kw_t;
 
-    n = BOR_ALLOC(plan_lisp_node_t);
+static kw_t kw[] = {
+    { "define", PLAN_PDDL_KW_DEFINE },
+    { "domain", PLAN_PDDL_KW_DOMAIN },
+    { ":domain", PLAN_PDDL_KW_DOMAIN },
+    { ":requirements", PLAN_PDDL_KW_REQUIREMENTS },
+    { ":types", PLAN_PDDL_KW_TYPES },
+    { ":predicates", PLAN_PDDL_KW_PREDICATES },
+    { ":action", PLAN_PDDL_KW_ACTION },
+    { ":parameters", PLAN_PDDL_KW_PARAMETERS },
+    { ":precondition", PLAN_PDDL_KW_PRE },
+    { ":effect", PLAN_PDDL_KW_EFF },
+
+    { ":strips", PLAN_PDDL_KW_STRIPS },
+    { ":typing", PLAN_PDDL_KW_TYPING },
+    { ":negative-preconditions", PLAN_PDDL_KW_NEGATIVE_PRE },
+    { ":disjunctive-preconditions", PLAN_PDDL_KW_DISJUNCTIVE_PRE },
+    { ":equality", PLAN_PDDL_KW_EQUALITY },
+    { ":existential-preconditions", PLAN_PDDL_KW_EXISTENTIAL_PRE },
+    { ":universal-preconditions", PLAN_PDDL_KW_UNIVERSAL_PRE },
+    { ":conditional-effects", PLAN_PDDL_KW_CONDITIONAL_EFF },
+    { ":numeric-fluents", PLAN_PDDL_KW_NUMERIC_FLUENT },
+    { ":numeric-fluents", PLAN_PDDL_KW_OBJECT_FLUENT },
+    { ":durative-actions", PLAN_PDDL_KW_DURATIVE_ACTION },
+    { ":duration-inequalities", PLAN_PDDL_KW_DURATION_INEQUALITY },
+    { ":continuous-effects", PLAN_PDDL_KW_CONTINUOUS_EFF },
+    { ":derived-predicates", PLAN_PDDL_KW_DERIVED_PRED },
+    { ":timed-initial-literals", PLAN_PDDL_KW_TIMED_INITIAL_LITERAL },
+    { ":durative-actions", PLAN_PDDL_KW_DURATIVE_ACTION },
+    { ":preferences", PLAN_PDDL_KW_PREFERENCE },
+    { ":constraints", PLAN_PDDL_KW_CONSTRAINT },
+    { ":action-costs", PLAN_PDDL_KW_ACTION_COST },
+    { ":multi-agent", PLAN_PDDL_KW_MULTI_AGENT },
+    { ":unfactored-privacy", PLAN_PDDL_KW_UNFACTORED_PRIVACY },
+    { ":factored_privacy", PLAN_PDDL_KW_FACTORED_PRIVACY },
+    { ":quantified-preconditions", PLAN_PDDL_KW_QUANTIFIED_PRE },
+    { ":fluents", PLAN_PDDL_KW_FLUENTS },
+    { ":adl", PLAN_PDDL_KW_ADL },
+    { ":multi-agent", PLAN_PDDL_KW_MULTI_AGENT },
+    { ":unfactored-privacy", PLAN_PDDL_KW_UNFACTORED_PRIVACY },
+    { ":factored-privacy", PLAN_PDDL_KW_FACTORED_PRIVACY },
+
+    { ":functions", PLAN_PDDL_KW_FUNCTIONS },
+    { "number", PLAN_PDDL_KW_NUMBER },
+    { "problem", PLAN_PDDL_KW_PROBLEM },
+    { ":objects", PLAN_PDDL_KW_OBJECTS },
+    { ":init", PLAN_PDDL_KW_INIT },
+    { ":goal", PLAN_PDDL_KW_GOAL },
+    { ":metric", PLAN_PDDL_KW_METRIC },
+    { "minimize", PLAN_PDDL_KW_MINIMIZE },
+    { "maximize", PLAN_PDDL_KW_MAXIMIZE },
+    { "increase", PLAN_PDDL_KW_INCREASE },
+
+    { "and", PLAN_PDDL_KW_AND },
+    { "or", PLAN_PDDL_KW_OR },
+    { "not", PLAN_PDDL_KW_NOT },
+    { "imply", PLAN_PDDL_KW_IMPLY },
+    { "exists", PLAN_PDDL_KW_EXISTS },
+    { "forall", PLAN_PDDL_KW_FORALL },
+    { "when", PLAN_PDDL_KW_WHEN },
+};
+static int kw_size = sizeof(kw) / sizeof(kw_t);
+
+static int recongnizeKeyword(const char *text)
+{
+    int i;
+
+    for (i = 0; i < kw_size; ++i){
+        if (strcmp(text, kw[i].text) == 0)
+            return kw[i].kw;
+    }
+
+    return -1;
+}
+
+static plan_pddl_lisp_node_t *lispNodeNew(void)
+{
+    plan_pddl_lisp_node_t *n;
+
+    n = BOR_ALLOC(plan_pddl_lisp_node_t);
     n->value = NULL;
+    n->kw = -1;
     borListInit(&n->children);
     borListInit(&n->list);
     return n;
 }
 
-static void lispNodeDel(plan_lisp_node_t *n)
+static void lispNodeDel(plan_pddl_lisp_node_t *n)
 {
     bor_list_t *item, *tmp;
-    plan_lisp_node_t *m;
+    plan_pddl_lisp_node_t *m;
 
     BOR_LIST_FOR_EACH_SAFE(&n->children, item, tmp){
-        m = BOR_LIST_ENTRY(item, plan_lisp_node_t, list);
+        m = BOR_LIST_ENTRY(item, plan_pddl_lisp_node_t, list);
         lispNodeDel(m);
     }
 
     BOR_FREE(n);
 }
 
-static plan_lisp_node_t *parseExp(char *data, int from, int size, int *cont)
+static plan_pddl_lisp_node_t *parseExp(char *data, int from, int size, int *cont)
 {
-    plan_lisp_node_t *exp, *sub;
+    plan_pddl_lisp_node_t *exp, *sub;
     int i = from;
     char c;
 
@@ -90,6 +171,7 @@ static plan_lisp_node_t *parseExp(char *data, int from, int size, int *cont)
 
             c = data[i];
             data[i] = 0x0;
+            sub->kw = recongnizeKeyword(sub->value);
         }
     }
 
@@ -103,7 +185,7 @@ plan_pddl_lisp_t *planPDDLLispParse(const char *fn)
     struct stat st;
     char *data;
     plan_pddl_lisp_t *lisp;
-    plan_lisp_node_t *root = NULL;
+    plan_pddl_lisp_node_t *root = NULL;
 
     fd = open(fn, O_RDONLY);
     if (fd == -1){
@@ -157,23 +239,23 @@ void planPDDLLispDel(plan_pddl_lisp_t *lisp)
     BOR_FREE(lisp);
 }
 
-static void nodeDump(const plan_lisp_node_t *node, FILE *fout, int prefix)
+static void nodeDump(const plan_pddl_lisp_node_t *node, FILE *fout, int prefix)
 {
     bor_list_t *item;
-    const plan_lisp_node_t *m;
+    const plan_pddl_lisp_node_t *m;
     int i;
 
     if (node->value != NULL){
         for (i = 0; i < prefix; ++i)
             fprintf(fout, " ");
-        fprintf(fout, "%s\n", node->value);
+        fprintf(fout, "%s [%d]\n", node->value, node->kw);
     }else{
         for (i = 0; i < prefix; ++i)
             fprintf(fout, " ");
         fprintf(fout, "(\n");
 
         BOR_LIST_FOR_EACH(&node->children, item){
-            m = BOR_LIST_ENTRY(item, plan_lisp_node_t, list);
+            m = BOR_LIST_ENTRY(item, plan_pddl_lisp_node_t, list);
             nodeDump(m, fout, prefix + 4);
         }
 
--- src/pddl_lisp.h
+++ src/pddl_lisp.h
@@ -8,15 +8,73 @@
 extern "C" {
 #endif /* __cplusplus */
 
-struct _plan_lisp_node_t {
+typedef enum {
+    PLAN_PDDL_KW_DEFINE = 1,
+    PLAN_PDDL_KW_DOMAIN,
+    PLAN_PDDL_KW_REQUIREMENTS,
+    PLAN_PDDL_KW_TYPES,
+    PLAN_PDDL_KW_CONSTANTS,
+    PLAN_PDDL_KW_PREDICATES,
+    PLAN_PDDL_KW_ACTION,
+    PLAN_PDDL_KW_PARAMETERS,
+    PLAN_PDDL_KW_PRE,
+    PLAN_PDDL_KW_EFF,
+
+    PLAN_PDDL_KW_STRIPS,
+    PLAN_PDDL_KW_TYPING,
+    PLAN_PDDL_KW_NEGATIVE_PRE,
+    PLAN_PDDL_KW_DISJUNCTIVE_PRE,
+    PLAN_PDDL_KW_EQUALITY,
+    PLAN_PDDL_KW_EXISTENTIAL_PRE,
+    PLAN_PDDL_KW_UNIVERSAL_PRE,
+    PLAN_PDDL_KW_CONDITIONAL_EFF,
+    PLAN_PDDL_KW_NUMERIC_FLUENT,
+    PLAN_PDDL_KW_OBJECT_FLUENT,
+    PLAN_PDDL_KW_DURATIVE_ACTION,
+    PLAN_PDDL_KW_DURATION_INEQUALITY,
+    PLAN_PDDL_KW_CONTINUOUS_EFF,
+    PLAN_PDDL_KW_DERIVED_PRED,
+    PLAN_PDDL_KW_TIMED_INITIAL_LITERAL,
+    PLAN_PDDL_KW_PREFERENCE,
+    PLAN_PDDL_KW_CONSTRAINT,
+    PLAN_PDDL_KW_ACTION_COST,
+    PLAN_PDDL_KW_MULTI_AGENT,
+    PLAN_PDDL_KW_UNFACTORED_PRIVACY,
+    PLAN_PDDL_KW_FACTORED_PRIVACY,
+    PLAN_PDDL_KW_QUANTIFIED_PRE,
+    PLAN_PDDL_KW_FLUENTS,
+    PLAN_PDDL_KW_ADL,
+
+    PLAN_PDDL_KW_FUNCTIONS,
+    PLAN_PDDL_KW_NUMBER,
+    PLAN_PDDL_KW_PROBLEM,
+    PLAN_PDDL_KW_OBJECTS,
+    PLAN_PDDL_KW_INIT,
+    PLAN_PDDL_KW_GOAL,
+    PLAN_PDDL_KW_METRIC,
+    PLAN_PDDL_KW_MINIMIZE,
+    PLAN_PDDL_KW_MAXIMIZE,
+    PLAN_PDDL_KW_INCREASE,
+
+    PLAN_PDDL_KW_AND,
+    PLAN_PDDL_KW_OR,
+    PLAN_PDDL_KW_NOT,
+    PLAN_PDDL_KW_IMPLY,
+    PLAN_PDDL_KW_EXISTS,
+    PLAN_PDDL_KW_FORALL,
+    PLAN_PDDL_KW_WHEN,
+} plan_pddl_kw_t;
+
+struct _plan_pddl_lisp_node_t {
     const char *value;
+    int kw;
     bor_list_t children;
     bor_list_t list;
 };
-typedef struct _plan_lisp_node_t plan_lisp_node_t;
+typedef struct _plan_pddl_lisp_node_t plan_pddl_lisp_node_t;
 
 struct _plan_pddl_lisp_t {
-    plan_lisp_node_t *root;
+    plan_pddl_lisp_node_t *root;
     int fd;
     char *data;
     size_t size;

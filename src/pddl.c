7bce3709fd9a4f26c9596f08f513e5c82fa958bb
Daniel Fiser <danfis@danfis.cz>
2015-06-22 11:52:47 +0200

    pddl: Requirements are parsed using keywords instead of string comparasion.

--- src/pddl.c
+++ src/pddl.c
@@ -23,58 +23,58 @@
 #include "pddl.h"
 
 struct _require_mask_t {
-    const char *name;
+    int kw;
     unsigned mask;
 };
 typedef struct _require_mask_t require_mask_t;
 
 static require_mask_t require_mask[] = {
-    { ":strips", PLAN_PDDL_REQUIRE_STRIPS },
-    { ":typing", PLAN_PDDL_REQUIRE_TYPING },
-    { ":negative-preconditions", PLAN_PDDL_REQUIRE_NEGATIVE_PRE },
-    { ":disjunctive-preconditions", PLAN_PDDL_REQUIRE_DISJUNCTIVE_PRE },
-    { ":equality", PLAN_PDDL_REQUIRE_EQUALITY },
-    { ":existential-preconditions", PLAN_PDDL_REQUIRE_EXISTENTIAL_PRE },
-    { ":universal-preconditions", PLAN_PDDL_REQUIRE_UNIVERSAL_PRE },
-    { ":conditional-effects", PLAN_PDDL_REQUIRE_CONDITIONAL_EFF },
-    { ":numeric-fluents", PLAN_PDDL_REQUIRE_NUMERIC_FLUENT },
-    { ":numeric-fluents", PLAN_PDDL_REQUIRE_OBJECT_FLUENT },
-    { ":durative-actions", PLAN_PDDL_REQUIRE_DURATIVE_ACTION },
-    { ":duration-inequalities", PLAN_PDDL_REQUIRE_DURATION_INEQUALITY },
-    { ":continuous-effects", PLAN_PDDL_REQUIRE_CONTINUOUS_EFF },
-    { ":derived-predicates", PLAN_PDDL_REQUIRE_DERIVED_PRED },
-    { ":timed-initial-literals", PLAN_PDDL_REQUIRE_TIMED_INITIAL_LITERAL },
-    { ":durative-actions", PLAN_PDDL_REQUIRE_DURATIVE_ACTION },
-    { ":preferences", PLAN_PDDL_REQUIRE_PREFERENCE },
-    { ":constraints", PLAN_PDDL_REQUIRE_CONSTRAINT },
-    { ":action-costs", PLAN_PDDL_REQUIRE_ACTION_COST },
-    { ":multi-agent", PLAN_PDDL_REQUIRE_MULTI_AGENT },
-    { ":unfactored-privacy", PLAN_PDDL_REQUIRE_UNFACTORED_PRIVACY },
-    { ":factored_privacy", PLAN_PDDL_REQUIRE_FACTORED_PRIVACY },
+    { PLAN_PDDL_KW_STRIPS, PLAN_PDDL_REQUIRE_STRIPS },
+    { PLAN_PDDL_KW_TYPING, PLAN_PDDL_REQUIRE_TYPING },
+    { PLAN_PDDL_KW_NEGATIVE_PRE, PLAN_PDDL_REQUIRE_NEGATIVE_PRE },
+    { PLAN_PDDL_KW_DISJUNCTIVE_PRE, PLAN_PDDL_REQUIRE_DISJUNCTIVE_PRE },
+    { PLAN_PDDL_KW_EQUALITY, PLAN_PDDL_REQUIRE_EQUALITY },
+    { PLAN_PDDL_KW_EXISTENTIAL_PRE, PLAN_PDDL_REQUIRE_EXISTENTIAL_PRE },
+    { PLAN_PDDL_KW_UNIVERSAL_PRE, PLAN_PDDL_REQUIRE_UNIVERSAL_PRE },
+    { PLAN_PDDL_KW_CONDITIONAL_EFF, PLAN_PDDL_REQUIRE_CONDITIONAL_EFF },
+    { PLAN_PDDL_KW_NUMERIC_FLUENT, PLAN_PDDL_REQUIRE_NUMERIC_FLUENT },
+    { PLAN_PDDL_KW_OBJECT_FLUENT, PLAN_PDDL_REQUIRE_OBJECT_FLUENT },
+    { PLAN_PDDL_KW_DURATIVE_ACTION, PLAN_PDDL_REQUIRE_DURATIVE_ACTION },
+    { PLAN_PDDL_KW_DURATION_INEQUALITY, PLAN_PDDL_REQUIRE_DURATION_INEQUALITY },
+    { PLAN_PDDL_KW_CONTINUOUS_EFF, PLAN_PDDL_REQUIRE_CONTINUOUS_EFF },
+    { PLAN_PDDL_KW_DERIVED_PRED, PLAN_PDDL_REQUIRE_DERIVED_PRED },
+    { PLAN_PDDL_KW_TIMED_INITIAL_LITERAL, PLAN_PDDL_REQUIRE_TIMED_INITIAL_LITERAL },
+    { PLAN_PDDL_KW_DURATIVE_ACTION, PLAN_PDDL_REQUIRE_DURATIVE_ACTION },
+    { PLAN_PDDL_KW_PREFERENCE, PLAN_PDDL_REQUIRE_PREFERENCE },
+    { PLAN_PDDL_KW_CONSTRAINT, PLAN_PDDL_REQUIRE_CONSTRAINT },
+    { PLAN_PDDL_KW_ACTION_COST, PLAN_PDDL_REQUIRE_ACTION_COST },
+    { PLAN_PDDL_KW_MULTI_AGENT, PLAN_PDDL_REQUIRE_MULTI_AGENT },
+    { PLAN_PDDL_KW_UNFACTORED_PRIVACY, PLAN_PDDL_REQUIRE_UNFACTORED_PRIVACY },
+    { PLAN_PDDL_KW_FACTORED_PRIVACY, PLAN_PDDL_REQUIRE_FACTORED_PRIVACY },
 
-    { ":quantified-preconditions", PLAN_PDDL_REQUIRE_EXISTENTIAL_PRE |
+    { PLAN_PDDL_KW_QUANTIFIED_PRE, PLAN_PDDL_REQUIRE_EXISTENTIAL_PRE |
                                    PLAN_PDDL_REQUIRE_UNIVERSAL_PRE },
-    { ":fluents", PLAN_PDDL_REQUIRE_NUMERIC_FLUENT |
-                  PLAN_PDDL_REQUIRE_OBJECT_FLUENT },
-    { ":adl", PLAN_PDDL_REQUIRE_STRIPS |
-              PLAN_PDDL_REQUIRE_TYPING |
-              PLAN_PDDL_REQUIRE_NEGATIVE_PRE |
-              PLAN_PDDL_REQUIRE_DISJUNCTIVE_PRE |
-              PLAN_PDDL_REQUIRE_EQUALITY |
-              PLAN_PDDL_REQUIRE_EXISTENTIAL_PRE |
-              PLAN_PDDL_REQUIRE_UNIVERSAL_PRE |
-              PLAN_PDDL_REQUIRE_CONDITIONAL_EFF },
+    { PLAN_PDDL_KW_FLUENTS, PLAN_PDDL_REQUIRE_NUMERIC_FLUENT |
+                            PLAN_PDDL_REQUIRE_OBJECT_FLUENT },
+    { PLAN_PDDL_KW_ADL, PLAN_PDDL_REQUIRE_STRIPS |
+                        PLAN_PDDL_REQUIRE_TYPING |
+                        PLAN_PDDL_REQUIRE_NEGATIVE_PRE |
+                        PLAN_PDDL_REQUIRE_DISJUNCTIVE_PRE |
+                        PLAN_PDDL_REQUIRE_EQUALITY |
+                        PLAN_PDDL_REQUIRE_EXISTENTIAL_PRE |
+                        PLAN_PDDL_REQUIRE_UNIVERSAL_PRE |
+                        PLAN_PDDL_REQUIRE_CONDITIONAL_EFF },
 };
 static int require_mask_size = sizeof(require_mask) / sizeof(require_mask_t);
 
 static char _object_type_name[] = "object";
 
-unsigned requireMask(const char *require)
+unsigned requireMask(int kw)
 {
     int i;
 
     for (i = 0; i < require_mask_size; ++i){
-        if (strcmp(require, require_mask[i].name) == 0)
+        if (require_mask[i].kw == kw)
             return require_mask[i].mask;
     }
     return 0u;
@@ -175,7 +175,7 @@ static unsigned parseRequire(plan_pddl_lisp_node_t *root)
             fprintf(stderr, "Error PDDL: Invalid :requirements definition.\n");
             return 0u;
         }
-        if ((m = requireMask(n->value)) == 0u){
+        if ((m = requireMask(n->kw)) == 0u){
             fprintf(stderr, "Error PDDL: Invalid :requirements definition:"
                             " Unknown keyword `%s'\n", n->value);
             return 0u;

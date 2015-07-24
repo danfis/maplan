824511bdf229d4bcc3aa2ecaa4223123146d5212
Daniel Fiser <danfis@danfis.cz>
2015-07-24 10:52:21 +0200

    pddl-predicate: Parsing :private predicates.

--- src/pddl_predicate.c
+++ src/pddl_predicate.c
@@ -24,6 +24,7 @@
 struct _set_t {
     plan_pddl_predicate_t *pred;
     const plan_pddl_types_t *types;
+    const char *owner_var;
 };
 typedef struct _set_t set_t;
 
@@ -34,6 +35,7 @@ static int setCB(const plan_pddl_lisp_node_t *root,
 {
     plan_pddl_predicate_t *pred = ((set_t *)ud)->pred;
     const plan_pddl_types_t *types = ((set_t *)ud)->types;
+    const char *owner_var = ((set_t *)ud)->owner_var;
     int i, j, tid;
 
     tid = 0;
@@ -49,8 +51,12 @@ static int setCB(const plan_pddl_lisp_node_t *root,
     j = pred->param_size;
     pred->param_size += child_to - child_from;
     pred->param = BOR_REALLOC_ARR(pred->param, int, pred->param_size);
-    for (i = child_from; i < child_to; ++i, ++j)
+    for (i = child_from; i < child_to; ++i, ++j){
         pred->param[j] = tid;
+        if (owner_var != NULL && strcmp(owner_var, root->child[i].value) == 0){
+            pred->owner_param = j;
+        }
+    }
     return 0;
 }
 
@@ -67,6 +73,7 @@ static int checkDuplicate(const plan_pddl_predicates_t *ps, const char *name)
 
 static int parsePredicate(const plan_pddl_types_t *types,
                           const plan_pddl_lisp_node_t *n,
+                          const char *owner_var,
                           const char *errname,
                           plan_pddl_predicates_t *ps)
 {
@@ -86,12 +93,47 @@ static int parsePredicate(const plan_pddl_types_t *types,
     p = planPDDLPredicatesAdd(ps);
     set.pred = p;
     set.types = types;
+    set.owner_var = owner_var;
     if (planPDDLLispParseTypedList(n, 1, n->child_size, setCB, &set) != 0){
         planPDDLPredicatesRemoveLast(ps);
         return -1;
     }
 
     p->name = n->child[0].value;
+    return 0;
+}
+
+static int parsePrivatePredicates(const plan_pddl_types_t *types,
+                                  const plan_pddl_lisp_node_t *n,
+                                  plan_pddl_predicates_t *ps)
+{
+    const char *owner_var;
+    int i, from;
+
+    if (n->child_size < 3
+            || n->child[0].kw != PLAN_PDDL_KW_PRIVATE
+            || n->child[1].value == NULL
+            || n->child[1].value[0] != '?'
+            || (n->child[2].value != NULL && n->child_size < 5)){
+        ERRN2(n, "Invalid definition of :private predicate.");
+        return -1;
+    }
+
+    owner_var = n->child[1].value;
+
+    if (n->child[2].value == NULL){
+        from = 2;
+    }else{
+        from = 4;
+    }
+    for (i = from; i < n->child_size; ++i){
+        if (parsePredicate(types, n->child + i, owner_var,
+                           "private predicate", ps) != 0)
+            return -1;
+
+        ps->pred[ps->size - 1].is_private = 1;
+    }
+
     return 0;
 }
 
@@ -112,7 +154,7 @@ int planPDDLPredicatesParse(const plan_pddl_lisp_t *domain,
                             plan_pddl_predicates_t *ps)
 {
     const plan_pddl_lisp_node_t *n;
-    int i;
+    int i, to, private;
 
     n = planPDDLLispFindNode(&domain->root, PLAN_PDDL_KW_PREDICATES);
     if (n == NULL)
@@ -121,16 +163,34 @@ int planPDDLPredicatesParse(const plan_pddl_lisp_t *domain,
     ps->eq_pred = -1;
     if (require & PLAN_PDDL_REQUIRE_EQUALITY)
         addEqPredicate(ps);
-    for (i = 1; i < n->child_size; ++i){
-        if (n->child[i].child_size > 0
-                && n->child[i].child[0].kw == PLAN_PDDL_KW_PRIVATE){
-            // Process :private predicates separately
-            continue;
 
+    // Determine if we can expect :private definitions
+    private = (require & PLAN_PDDL_REQUIRE_UNFACTORED_PRIVACY)
+                || (require & PLAN_PDDL_REQUIRE_FACTORED_PRIVACY);
+
+    if (private){
+        // Find out first :private definition
+        for (to = 1; to < n->child_size; ++to){
+            if (n->child[to].child_size > 0
+                    && n->child[to].child[0].kw == PLAN_PDDL_KW_PRIVATE)
+                break;
         }
+    }else{
+        to = n->child_size;
+    }
 
-        if (parsePredicate(types, n->child + i, "predicate", ps) != 0)
+    // Parse non :private predicates
+    for (i = 1; i < to; ++i){
+        if (parsePredicate(types, n->child + i, NULL, "predicate", ps) != 0)
             return -1;
+    }
+
+    if (private){
+        // Parse :private predicates
+        for (i = to; i < n->child_size; ++i){
+            if (parsePrivatePredicates(types, n->child + i, ps) != 0)
+                return -1;
+        }
     }
 
     return 0;
@@ -148,7 +208,7 @@ int planPDDLFunctionsParse(const plan_pddl_lisp_t *domain,
         return 0;
 
     for (i = 1; i < n->child_size; ++i){
-        if (parsePredicate(types, n->child + i, "function", ps) != 0)
+        if (parsePredicate(types, n->child + i, NULL, "function", ps) != 0)
             return -1;
 
         if (i + 2 < n->child_size

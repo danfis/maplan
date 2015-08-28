d87fa6db3b771d66ba2b9364cb6ae14e2efb2b90
Daniel Fiser <danfis@danfis.cz>
2015-08-28 12:26:15 +0200

    heur-ma-potential: Distributed heuristics.

--- src/heur_ma_potential.c
+++ src/heur_ma_potential.c
@@ -20,25 +20,14 @@
 #include <boruvka/alloc.h>
 #include "plan/heur.h"
 
-struct _prob_t {
-    plan_pot_prob_t prob;
-    int *state_var_id;
-    int state_var_id_size;
-};
-typedef struct _prob_t prob_t;
-
-static prob_t *probNew(int size);
-static void probDel(prob_t *prob, int size);
-
 struct _plan_heur_ma_potential_t {
     plan_heur_t heur;
     plan_pot_t pot;
 
     int pot_pending;
-    prob_t *pot_prob;
+    plan_pot_prob_t *pot_prob;
     int pot_prob_size;
-    int *pot_state_var_id;
-    int pot_state_var_id_size;
+    plan_state_t *state;
 };
 typedef struct _plan_heur_ma_potential_t plan_heur_ma_potential_t;
 #define HEUR(parent) \
@@ -56,17 +45,23 @@ static void potStart(plan_heur_ma_potential_t *h, plan_ma_comm_t *comm,
                      const plan_state_t *state);
 static int potUpdate(plan_heur_ma_potential_t *h, plan_ma_comm_t *comm,
                      const plan_ma_msg_t *msg);
-static void potCompute(plan_heur_ma_potential_t *h);
+static int potCompute(plan_heur_ma_potential_t *h);
 
 plan_heur_t *planHeurMAPotentialNew(const plan_problem_t *p)
 {
     plan_heur_ma_potential_t *h;
+    PLAN_STATE_STACK(state, p->state_pool->num_vars);
 
     h = BOR_ALLOC(plan_heur_ma_potential_t);
+    bzero(h, sizeof(*h));
     _planHeurInit(&h->heur, heurDel, NULL, NULL);
     _planHeurMAInit(&h->heur, heurHeur, NULL, heurUpdate, heurRequest);
-    planPotInit(&h->pot, p->var, p->var_size, p->goal, p->op, p->op_size, 0);
+
+    planStatePoolGetState(p->state_pool, p->initial_state, &state);
+    planPotInit(&h->pot, p->var, p->var_size, p->goal,
+                p->op, p->op_size, &state, 0);
     h->pot_pending = -1;
+    h->state = planStateNew(p->state_pool->num_vars);
 
     return &h->heur;
 }
@@ -74,6 +69,7 @@ plan_heur_t *planHeurMAPotentialNew(const plan_problem_t *p)
 static void heurDel(plan_heur_t *heur)
 {
     plan_heur_ma_potential_t *h = HEUR(heur);
+    planStateDel(h->state);
     planPotFree(&h->pot);
     _planHeurFree(&h->heur);
     BOR_FREE(h);
@@ -85,11 +81,14 @@ static int heurHeur(plan_heur_t *heur, plan_ma_comm_t *comm,
     plan_heur_ma_potential_t *h = HEUR(heur);
 
     if (h->pot.pot == NULL){
+        planStateCopy(h->state, state);
         potStart(h, comm, state);
         return -1;
     }
 
     // TODO
+    res->heur = planPotStatePot(&h->pot, state);
+    res->heur = BOR_MAX(res->heur, 0);
     return 0;
 }
 
@@ -99,8 +98,7 @@ static int heurUpdate(plan_heur_t *heur, plan_ma_comm_t *comm,
     plan_heur_ma_potential_t *h = HEUR(heur);
 
     if (potUpdate(h, comm, msg) == 0){
-        potCompute(h);
-        // TODO
+        res->heur = potCompute(h);
         return 0;
     }
 
@@ -125,7 +123,7 @@ static void heurRequest(plan_heur_t *heur, plan_ma_comm_t *comm,
     mout = planMAMsgNew(PLAN_MA_MSG_HEUR, PLAN_MA_MSG_HEUR_POT_RESPONSE,
                         comm->node_id);
     planMAStateGetFromMAMsg(h->heur.ma_state, msg, &state);
-    planMAMsgSetPotProb(mout, &h->pot, &state);
+    planMAMsgSetPotProb(mout, &h->pot);
     planMACommSendToNode(comm, planMAMsgAgent(msg), mout);
     planMAMsgDel(mout);
 }
@@ -143,17 +141,12 @@ static void potStart(plan_heur_ma_potential_t *h, plan_ma_comm_t *comm,
 
     h->pot_pending = comm->node_size - 1;
     h->pot_prob_size = h->pot_pending;
-    h->pot_prob = probNew(h->pot_prob_size);
-    h->pot_state_var_id = BOR_ALLOC_ARR(int, h->pot.var_size);
-    h->pot_state_var_id_size = planPotToVarIds(&h->pot, state,
-                                               h->pot_state_var_id);
+    h->pot_prob = BOR_CALLOC_ARR(plan_pot_prob_t, h->pot_prob_size);
 }
 
 static int potUpdate(plan_heur_ma_potential_t *h, plan_ma_comm_t *comm,
                      const plan_ma_msg_t *msg)
 {
-    prob_t *prob;
-
     if (planMAMsgType(msg) != PLAN_MA_MSG_HEUR
             || planMAMsgSubType(msg) != PLAN_MA_MSG_HEUR_POT_RESPONSE){
         fprintf(stderr, "[%d] Unexpected message: %d/%d\n",
@@ -162,11 +155,7 @@ static int potUpdate(plan_heur_ma_potential_t *h, plan_ma_comm_t *comm,
         return -1;
     }
 
-    prob = h->pot_prob + h->pot_pending - 1;
-    planMAMsgGetPotProb(msg, &prob->prob);
-    prob->state_var_id_size = planMAMsgPotProbStateVarIdSize(msg);
-    prob->state_var_id = BOR_ALLOC_ARR(int, prob->state_var_id_size);
-    planMAMsgPotProbStateVarId(msg, prob->state_var_id);
+    planMAMsgGetPotProb(msg, h->pot_prob + h->pot_pending - 1);
 
     --h->pot_pending;
     if (h->pot_pending == 0)
@@ -176,7 +165,8 @@ static int potUpdate(plan_heur_ma_potential_t *h, plan_ma_comm_t *comm,
 
 static int probMergeConstr(plan_pot_constr_t **dst, int *dst_size,
                            plan_pot_constr_t *src, int src_size,
-                           int private_start_id, int offset)
+                           int private_start_id, int offset,
+                           int rewrite)
 {
     plan_pot_constr_t *d, *s;
     int i, j, from, max_id = 0;
@@ -186,9 +176,18 @@ static int probMergeConstr(plan_pot_constr_t **dst, int *dst_size,
     *dst = BOR_REALLOC_ARR(*dst, plan_pot_constr_t, *dst_size);
     for (i = 0; i < src_size; ++i){
         d = (*dst) + i + from;
-        s = (*dst) + i;
+        s = src + i;
         *d = *s;
-        bzero(s, sizeof(*s));
+
+        if (rewrite){
+            bzero(s, sizeof(*s));
+
+        }else{
+            d->var_id = BOR_ALLOC_ARR(int, d->coef_size);
+            d->coef = BOR_ALLOC_ARR(int, d->coef_size);
+            memcpy(d->var_id, s->var_id, sizeof(int) * d->coef_size);
+            memcpy(d->coef, s->coef, sizeof(int) * d->coef_size);
+        }
 
         for (j = 0; j < d->coef_size; ++j){
             if (d->var_id[j] >= private_start_id)
@@ -200,64 +199,107 @@ static int probMergeConstr(plan_pot_constr_t **dst, int *dst_size,
     return max_id + 1;
 }
 
-static int probMerge(plan_pot_prob_t *prob, plan_pot_prob_t *merge,
-                     int private_start_id, int offset)
+static int probMergeGoal(plan_pot_constr_t *dst,
+                         const plan_pot_constr_t *src,
+                         int private_start_id, int offset)
 {
-    int size = 0, siz;
+    int i, j, max_id = 0, var_id;
 
-    // TODO: prob->goal
+    for (i = 0; i < src->coef_size; ++i){
+        var_id = src->var_id[i];
+        if (var_id >= private_start_id)
+            var_id += offset;
+
+        max_id = BOR_MAX(max_id, var_id);
+
+        for (j = 0; j < dst->coef_size; ++j){
+            if (var_id == dst->var_id[j])
+                break;
+        }
+
+        if (j == dst->coef_size){
+            ++dst->coef_size;
+            dst->coef = BOR_REALLOC_ARR(dst->coef, int, dst->coef_size);
+            dst->var_id = BOR_REALLOC_ARR(dst->var_id, int, dst->coef_size);
+            dst->var_id[dst->coef_size - 1] = var_id;
+            dst->coef[dst->coef_size - 1] = src->coef[i];
+        }
+    }
+
+    for (j = 0; j < dst->coef_size; ++j)
+        max_id = BOR_MAX(max_id, dst->var_id[j]);
+
+    return max_id + 1;
+}
+
+static int probMerge(plan_pot_prob_t *prob, plan_pot_prob_t *merge,
+                     int private_start_id, int offset, int rewrite)
+{
+    int i, var_id, size = 0, siz;
+
+    size = probMergeGoal(&prob->goal, &merge->goal, private_start_id, offset);
+
     siz = probMergeConstr(&prob->op, &prob->op_size,
                           merge->op, merge->op_size,
-                          private_start_id, offset);
+                          private_start_id, offset, rewrite);
     size = BOR_MAX(size, siz);
 
     siz = probMergeConstr(&prob->maxpot, &prob->maxpot_size,
                           merge->maxpot, merge->maxpot_size,
-                          private_start_id, offset);
+                          private_start_id, offset, rewrite);
     size = BOR_MAX(size, siz);
+
+    if (prob->var_size < size){
+        siz = prob->var_size;
+        prob->var_size = size;
+        prob->state_coef = BOR_REALLOC_ARR(prob->state_coef, int,
+                                           prob->var_size);
+        bzero(prob->state_coef + siz, sizeof(int) * (size - siz));
+    }
+
+    for (i = 0; i < merge->var_size; ++i){
+        var_id = i;
+        if (var_id >= private_start_id)
+            var_id += offset;
+        if (merge->state_coef[i] == 1)
+            prob->state_coef[var_id] = 1;
+    }
 
     return size;
 }
 
-static void potCompute(plan_heur_ma_potential_t *h)
+static int potCompute(plan_heur_ma_potential_t *h)
 {
     plan_pot_prob_t prob;
-    int i, size, var_size, offset;
+    int i, size, var_size, offset, heur = 0;
+    double dheur;
 
     bzero(&prob, sizeof(prob));
 
-    var_size = probMerge(&prob, &h->pot.prob, h->pot.lp_var_private, 0);
+    var_size = probMerge(&prob, &h->pot.prob, h->pot.lp_var_private, 0, 0);
     for (i = 0; i < h->pot_prob_size; ++i){
         offset = var_size - h->pot.lp_var_private;
-        size = probMerge(&prob, &h->pot_prob[i].prob,
-                         h->pot.lp_var_private, offset);
+        size = probMerge(&prob, h->pot_prob + i,
+                         h->pot.lp_var_private, offset, 1);
         var_size = BOR_MAX(var_size, size);
     }
 
-    probDel(h->pot_prob, h->pot_prob_size);
+    h->pot.pot = BOR_ALLOC_ARR(double, prob.var_size);
+    planPotCompute2(&prob, h->pot.pot);
+
+    for (dheur = 0., i = 0; i < prob.var_size; ++i){
+        if (prob.state_coef[i] > 0)
+            dheur += h->pot.pot[i];
+    }
+    heur = dheur;
+    heur = BOR_MAX(heur, 0);
+
+    planPotProbFree(&prob);
+    for (i = 0; i < h->pot_prob_size; ++i)
+        planPotProbFree(h->pot_prob + i);
+    BOR_FREE(h->pot_prob);
     h->pot_prob = NULL;
     h->pot_prob_size = 0;
-}
 
-
-
-static prob_t *probNew(int size)
-{
-    return BOR_CALLOC_ARR(prob_t, size);
-}
-
-static void probDel(prob_t *prob, int size)
-{
-    int i;
-
-    if (!prob)
-        return;
-
-    for (i = 0; i < size; ++i){
-        planPotProbFree(&prob[i].prob);
-        if (prob[i].state_var_id)
-            BOR_FREE(prob[i].state_var_id);
-    }
-
-    BOR_FREE(prob);
+    return heur;
 }
